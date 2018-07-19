6a9fad30be732489a10447542a775c5b9bff6800
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 02:08:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(people, name)
   people << name
-  puts "Welcome, #{name}. You are number #{people[name].index?}"
+  puts "Welcome, #{name}. You are number"
 end
 
 def now_serving
