4b64d4534478fd548be42114b8f433574c8b2319
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 01:51:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,10 @@ katz_deli = []
 def line(name)
   if name.empty?
     puts "The line is currently empty."
-  end 
+  else
+    name.each_with_index do |name, position|
+      current_line << "#{position + 1}. #{name}"
+  end
 
 end
 
